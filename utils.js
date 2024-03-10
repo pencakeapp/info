15ef12bed35c361ca33a0ebfb902167d13ee55fd
Suchul Shin <eva317@gmail.com>
2024-03-10 11:23:38 +0900

    update the version code

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
-var MAC_VERSION = "0.2.1";
-var WIN_VERSION = "0.2.1";
+var MAC_VERSION = "0.2.2";
+var WIN_VERSION = "0.2.2";
 
 var baseUrl = "https://github.com/pencakeapp-desktop/app/releases/download";
 
